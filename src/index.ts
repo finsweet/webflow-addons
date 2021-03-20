94ea71bffc5cf9de75dfef1856743e080d16cace
Alex Iglesias <alexiglesias@finsweet.com>
2021-03-20 17:00:08 +0100

    Delete index.ts

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import EditorFriendlyLinkBlocks from './editor-friendly-link-blocks/index';
-
-declare global {
-  interface Window {
-    WebflowHacks: { [key: string]: any };
-  }
-}
-
-window.WebflowHacks = { EditorFriendlyLinkBlocks };
