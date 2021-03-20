2a7603b36f5887f8bd6d55242385045ad42176a4
Alex Iglesias <alexiglesias@finsweet.com>
2021-03-20 17:00:40 +0100

    Added findTextNode

--- src/utils.ts/helpers.ts
+++ src/utils.ts/helpers.ts
@@ -2,5 +2,20 @@
  * Checks if an element is visible
  * @param {HTMLElement} element
  */
-export const isVisible = (element: HTMLElement) =>
+export const isVisible = (element: HTMLElement): boolean =>
   !!(element.offsetWidth || element.offsetHeight || element.getClientRects().length);
+
+/**
+ * Find the first text node child of an element
+ * @param element
+ */
+export const findTextNode = (element: Node): ChildNode | undefined => {
+  let textNode: ChildNode | undefined;
+
+  for (const node of element.childNodes) {
+    if (node.childNodes.length) textNode = findTextNode(node);
+    if (node.nodeType == Node.TEXT_NODE) textNode = node;
+    if (textNode) break;
+  }
+  return textNode;
+};
