3e94c80227727568eaff0eef3049dd573fbfaf80
Alex Iglesias <alexiglesias@finsweet.com>
2021-03-21 21:46:55 +0100

    Update infinite-slider.ts

--- src/infinite-slider/infinite-slider.ts
+++ src/infinite-slider/infinite-slider.ts
@@ -3,43 +3,34 @@ import { cloneNode } from '../utils.ts/helpers';
 document.addEventListener('DOMContentLoaded', () => {
   const sliders = document.querySelectorAll<HTMLDivElement>('.w-slider');
 
-  const handleSlider = (slider: HTMLDivElement) => {
+  for (const slider of sliders) {
     const mask = slider.querySelector('.w-slider-mask') as HTMLDivElement;
-    if (mask.clientWidth === slider.clientWidth) return;
+    if (mask.clientWidth === slider.clientWidth) continue;
 
     const slides = slider.getElementsByClassName('w-slide') as HTMLCollectionOf<HTMLDivElement>;
-    const slidesAmount = slides.length;
-    if (!slidesAmount) return;
+    const originalSlidesLength = slides.length;
 
-    // const fullyVisibleSlides = (() => {
-    //   const style = window.getComputedStyle(slides[0]);
-    //   const slideWidth = slides[0].offsetWidth + parseFloat(style.marginLeft) + parseFloat(style.marginRight);
-
-    //   return Math.floor(slider.clientWidth / slideWidth);
-    // })();
-
-    if (slides.length <= Math.floor(slider.clientWidth / mask.clientWidth)) return;
+    // If no slides or there isn't at least one non-visible slide, return.
+    if (!originalSlidesLength || originalSlidesLength <= Math.floor(slider.clientWidth / mask.clientWidth)) continue;
 
     [...slides].forEach((currentSlide) =>
       currentSlide.addEventListener('transitionstart', () => {
         if (currentSlide.getAttribute('aria-hidden')) return;
 
-        const slidesArray = [...slides];
-        const previousSlideIndex = slidesArray.findIndex((slide) => slide === currentSlide) - 1;
+        const totalSlides = [...slides];
+        const previousSlideIndex = totalSlides.findIndex((slide) => slide === currentSlide) - 1;
 
         if (previousSlideIndex >= 0) {
           const newSlide = cloneNode(slides[previousSlideIndex]);
           newSlide.style.transform = '';
           mask.appendChild(newSlide);
 
-          const slidesToTransform = [...slides].slice(slidesAmount);
+          const slidesToTransform = [...slides].slice(originalSlidesLength);
           slidesToTransform.forEach((slide) => (slide.style.transform = currentSlide.style.transform));
         } else {
-          slidesArray.slice(slidesAmount).forEach((slide) => slide.remove());
+          totalSlides.slice(originalSlidesLength).forEach((slide) => slide.remove());
         }
       })
     );
-  };
-
-  sliders.forEach((slider) => handleSlider(slider));
+  }
 });
