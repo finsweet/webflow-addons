7af004e20e244f24cbe43b20120578cfeff908e3
Alex Iglesias <alexiglesias@finsweet.com>
2021-03-21 20:17:49 +0100

    Added cloneNode

--- src/utils.ts/helpers.ts
+++ src/utils.ts/helpers.ts
@@ -19,3 +19,9 @@ export const findTextNode = (element: Node): ChildNode | undefined => {
   }
   return textNode;
 };
+
+/**
+ * Clone a node that has the same type as the original one
+ * @param node
+ */
+export const cloneNode = <T extends Node>(node: T): T => <T>node.cloneNode(true);
