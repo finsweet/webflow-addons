0534b5a3b14d2e8ab7e64bd41015c0104e96fb27
Alex Iglesias <alexiglesias@finsweet.com>
2021-03-21 20:17:41 +0100

    Update infinite-slider.ts

--- src/infinite-slider/infinite-slider.ts
+++ src/infinite-slider/infinite-slider.ts
@@ -11,33 +11,32 @@ document.addEventListener('DOMContentLoaded', () => {
     const slidesAmount = slides.length;
     if (!slidesAmount) return;
 
-    // [...slides].forEach((slide) => mask.appendChild(cloneNode(slide)));
-    // for (let index = 0; slides.lengt)
+    // const fullyVisibleSlides = (() => {
+    //   const style = window.getComputedStyle(slides[0]);
+    //   const slideWidth = slides[0].offsetWidth + parseFloat(style.marginLeft) + parseFloat(style.marginRight);
+
+    //   return Math.floor(slider.clientWidth / slideWidth);
+    // })();
+
+    if (slides.length <= Math.floor(slider.clientWidth / mask.clientWidth)) return;
 
     [...slides].forEach((currentSlide) =>
       currentSlide.addEventListener('transitionstart', () => {
         if (currentSlide.getAttribute('aria-hidden')) return;
 
         const slidesArray = [...slides];
-
         const previousSlideIndex = slidesArray.findIndex((slide) => slide === currentSlide) - 1;
-        console.log({ currentSlide, previousSlideIndex });
+
         if (previousSlideIndex >= 0) {
           const newSlide = cloneNode(slides[previousSlideIndex]);
           newSlide.style.transform = '';
           mask.appendChild(newSlide);
+
           const slidesToTransform = [...slides].slice(slidesAmount);
-          console.log({ slidesToTransform, transform: currentSlide.style.transform });
           slidesToTransform.forEach((slide) => (slide.style.transform = currentSlide.style.transform));
         } else {
           slidesArray.slice(slidesAmount).forEach((slide) => slide.remove());
         }
-      })
-    );
-
-    [...slides].forEach((slide) =>
-      slide.addEventListener('transitionend', () => {
-        if (!slide.getAttribute('aria-hidden')) console.log(slide);
       })
     );
   };
