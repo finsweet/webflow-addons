36a7c39076e07a115ea8f15fdb780d33806235d3
Alex Iglesias <alexiglesias@finsweet.com>
2021-04-10 00:24:21 +0200

    Fixed typo in description

--- display-style-values/index.ts
+++ display-style-values/index.ts
@@ -1,6 +1,6 @@
 /**
  * Copy text to clipboard through simple custom attributes.
- * @attribute [data-display-style] To set what style value to display. Example: data-display-style="fontSize"
+ * @attribute [data-display-style] To set what style value to display. Example: data-display-style="font-size"
  * @attribute [data-display-target] OPTIONAL Target to render the display value. If not provided, the same element with the data-display-style will act as the target. Example: data-display-target="#target-id"
  */
 const initDisplayStyleValues = (): void => {
